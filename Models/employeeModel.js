06fee4921f39e1c956789d48c444d6c355e82ed4
Ahmed Hassan <121809544+hassan9810@users.noreply.github.com>
2023-02-11 13:54:00 +0200

    Add files via upload

---
+++ Models/employeeModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+const personSchema = require("./personModel");
+
+/*** crete schema for employees collection ***/
+
+const employeeSchema = new mongoose.Schema(
+  Object.assign({}, personSchema.obj, {
+    _id: { type: Number },
+    _clinic: { type: Number, ref: "clinic" },
+    _monthlyRate: { type: Number },
+    _workingHours: { type: Number, min: 0, max: 24 },
+  })
+);
+
+/*** auto increment for _id field ***/
+employeeSchema.plugin(AutoIncrement, {
+  id: "employee_seq",
+  inc_field: "_id",
+  start_seq: 1000,
+});
+
+/*** mapping schema bind collection  ***/
+module.exports = mongoose.model("employee", employeeSchema);
---
+++ Routes/appointmentRouter.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const controller = require("../Controller/appointmentController");
+const validatorMiddleware = require("../Middlewares/errorValidation");
+const {
+  validateAppointment,
+  validatePatchAppointment,
+  numberIdParamsValidation,
+} = require("../Middlewares/validateData");
+const authorizationMW = require("../Middlewares/authorizationMW");
+
+const router = express.Router();
+
+router
+  .route("/appointment")
+  .all(authorizationMW.accessClinicResources("receptionist"))
+  .get(controller.getAllAppointments)
+  .post(validateAppointment, validatorMiddleware, controller.addAppointment);
+
+router
+  .route("/appointmentReports/all")
+  .get(authorizationMW.access(), controller.allAppointmentsReports);
+
+router
+  .route("/appointmentReports/daily")
+  .get(authorizationMW.access(), controller.dailyAppointmentsReports);
+
+router
+  .route("/appointmentReports/patient/:id")
+  .get(authorizationMW.access(), controller.patientAppointmentsReports);
+
+router
+  .route("/appointmentReports/doctor/:id")
+  .get(authorizationMW.access(), controller.doctorAppointmentsReports);
+
+router
+  .route("/appointment/:id")
+  .all(
+    numberIdParamsValidation,
+    validatorMiddleware,
+    authorizationMW.accessClinicResources("receptionist")
+  )
+  .get(controller.getAppointmentById)
+  .patch(
+    validatePatchAppointment,
+    validatorMiddleware,
+    controller.patchAppointment
+  )
+  .delete(controller.removeAppointmentById);
+
+module.exports = router;
